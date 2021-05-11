b4270042f513343214b68706edd722b3db597ffb
r(0_o)t <50882504+aerosol-can@users.noreply.github.com>
2021-05-11 16:35:27 +0530

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,12 @@
+Echo off
+pip3 install colorama
+pip3 install wikipedia
+pip3 install wolframalpha 
+pip3 install speechrecognition
+pip3 install pyttsx3
+cls
+Echo ...
+Echo ...
+Echo    All Requirements Satisfied!
+Echo    Setup Seems to be Completed!
+Echo on
